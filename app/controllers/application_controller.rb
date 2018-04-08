1e2767b627f821162de8ef42e9b98b10579d68b3
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-04-08 20:45:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    
+
     user = User.new(username: params[:username], password: params[:password])
 
     if user.save
@@ -60,7 +60,7 @@ end
   post '/login' do
     user = User.find_by(username: params[:username])
 
-    if user && user.authentication(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
 
        redirect "/account"
